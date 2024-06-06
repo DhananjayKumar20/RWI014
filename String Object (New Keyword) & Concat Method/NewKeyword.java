20e5d90d5803ad44761b06870362e195ab0af42b
Dhananjay Kumar <156174626+DhananjayKumar20@users.noreply.github.com>
2024-06-06 17:46:22 +0530

    Create NewKeyword.java

---
+++ String Object (New Keyword) & Concat Method/NewKeyword.java
@@ -0,0 +1,18 @@
+package My;
+
+public class NewKeyword 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		String a = new String("Rishab");   //New Keyword
+		System.out.println(a);
+		
+		String b = new String("Rishab");   //New Keyword
+		System.out.println(b);
+		
+		a=a.concat(" Kumar ");
+		System.out.println(a);
+	}
+}
