82399129ee2b4725ba44c2cb15ca89a97820c3d6
Dhananjay Kumar <156174626+DhananjayKumar20@users.noreply.github.com>
2024-06-10 17:27:27 +0530

    Create A.java
    This is an exception handing program.

---
+++ Exception handing Example/A.java
@@ -0,0 +1,19 @@
+package My;
+public class A 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int a=2, b=0,c;
+		try
+		{
+			c=(a/b);
+			System.out.println(c);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}	
+		System.out.println(" Main method ended ");
+	}
+}
